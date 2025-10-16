73ecc3b80fec198105e19a2516e487976fe33400
wtracy15 <117859071+wtracy15@users.noreply.github.com>
2025-10-16 13:56:51 -0400

    Like/unlike posts
    Added the ability to like and unlike all posts and change the like counter, and changed the distance between the like count and post comment elements.

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 const postsContainer = document.getElementById('posts-container')
+
 const posts = [
     {
+        id: 0,
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
+        liked: false,
         likes: 21
     },
     {
+        id: 1,
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
+        liked: false,
         likes: 4
     },
-        {
+    {
+        id: 2,
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        liked: false,
         likes: 152
     }
 ]
 
-posts.forEach(post => {
+
+posts.forEach((post, index) => {
     postsContainer.innerHTML += `
         <div class="post">
             <div class="post-header">
@@ -41,12 +49,30 @@ posts.forEach(post => {
             </div>
             <img class="post-img" src="${post.post}">
             <div class="interact-bar">
-                <img id="heart-btn" src="images/icon-heart.png">
+                <img class="heart-btn" src="images/icon-heart.png">
                 <img src="images/icon-comment.png">
                 <img src="images/icon-dm.png">
             </div>
-            <p class="like-count">${post.likes} likes</p>
+            <p class="like-count" id="like-count-${post.id}">${post.likes} likes</p>
             <p class="post-comment"><span>${post.username}</span> ${post.comment}</p>
         </div>
     `
+})
+
+const heartBtns = document.querySelectorAll('.heart-btn')
+
+heartBtns.forEach((heartBtn, index) => {
+    heartBtn.postObj = posts[index]
+    heartBtn.addEventListener('click', function(e){
+        target = e.target
+        if(!target.postObj.liked){
+            target.src = "images/icon-heart-liked.png";
+            target.postObj.likes++
+        } else {
+            target.src= "images/icon-heart.png";
+            target.postObj.likes--
+        }
+        target.postObj.liked = !target.postObj.liked;
+        document.getElementById(`like-count-${target.postObj.id}`).textContent = `${target.postObj.likes} likes`
+    })
 })
